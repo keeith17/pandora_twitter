fb107c1f35d3c1263bb4dd847e9ac6fcf7f569f3
smm_gabin <keeith17@github.com>
2024-09-04 12:03:51 +0900

    메시지 박스 크기 고정

--- twitter/src/component/message/chatRoomStyle.ts
+++ twitter/src/component/message/chatRoomStyle.ts
@@ -16,6 +16,8 @@ export const ChatRoomWrap = styled.div`
         img {
             width: 100%;
             border-radius: 50%;
+            aspect-ratio: 1/1;
+            object-fit: cover;
         }
     }
     .chatInfo {
